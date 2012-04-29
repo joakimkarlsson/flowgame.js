b20153eb7ab4d4f018af8498664b01be35472274
Joakim Karlsson <joakim@jkarlsson.com>
2012-04-29 13:05:49 +0200

    Working on moving tasks

--- public/javascripts/flowgame.js
+++ public/javascripts/flowgame.js
@@ -15,6 +15,17 @@ FlowGame.createTask = function(params) {
   };
 };
 
+
+FlowGame.createBoard = function(params) {
+  var param = params || {};
+  param.columns = param.columns || [];
+  param.columns.concat(FlowGame.createColumn({wip: Infinity}));
+
+  return {
+    columns: param.columns
+  };
+};
+
 FlowGame.workOnTask = function(task, calculateProgress) {
   var workedOnTask = FlowGame.cloneTask(task);
   workedOnTask.progress = calculateProgress(task);
@@ -35,6 +46,41 @@ FlowGame.addTaskToColumn = function(task, column) {
   return clone;
 };
 
+FlowGame.changeColumn = function(params) {
+  var len = params.columns.length;
+  var res = [];
+
+  for(var i = len - 1; i >= 0; i--) {
+    if(params.index === i) {
+      res.push(params.column);
+    }
+    else {
+      res.push(params.columns[i]);
+    }
+  }
+
+  return res;
+};
+
+FlowGame.changeColumnsOnBoard = function(params) {
+  var board = FlowGame.cloneBoard(params.board);
+  board.columns = params.columns;
+  return board;
+};
+
+FlowGame.addTaskToBoard = function(params) {
+  debugger;
+  var column = FlowGame.addTaskToColumn(params.task, params.board.columns[params.column]);
+  var columns = FlowGame.changeColumn({column: column, columns: params.board.columns, index: params.column});
+  return FlowGame.changeColumnsOnBoard({board: params.board, columns: columns});
+};
+
+
+FlowGame.moveCompletedTasksOnBoard = function(board) {
+  var columns = FlowGame.moveCompletedTasksForward(board.columns);
+  return FlowGame.changeColumnsOnBoard({board: board, columns: columns});
+};
+
 FlowGame.cloneColumn = function(column) {
   return {
     tasks: column.tasks.slice()
@@ -46,4 +92,30 @@ FlowGame.cloneTask = function(task) {
     size: task.size,
     progress: task.progress
   };
+};
+
+FlowGame.cloneBoard = function(board) {
+  return {
+    columns: board.columns.slice()
+  };
+};
+
+FlowGame.moveCompletedTasksForward = function(columns) {
+  if(columns.length === 2) {
+    return FlowGame.moveTasksBetweenColumns(columns[0], columns[1]);
+  }
+
+  var first = columns[0];
+  var rest = FlowGame.moveCompletedTasksForward(columns.slice(1));
+
+  return FlowGame.moveTasksBetweenColumns(first, rest[0]);
+};
+
+FlowGame.moveTasksBetweenColumns = function(sourceColumn, destinationColumn) {
+    var tasksToMove = FlowGame.completedTasksInColumn(sourceColumn);
+
+    var updatedSourceColumn = FlowGame.removeTasksFromColumn(tasksToMove, sourceColumn);
+    var updatedDestinationColumn = FlowGame.addTasksToColumn(tasksToMove, destinationColumn);
+
+    return [updatedSourceColumn, updatedDestinationColumn];
 };
---
+++ spec/javascripts/board_spec.js
@@ -0,0 +1,25 @@
+describe("Board", function() {
+  var board = {};
+  var task = {};
+
+  beforeEach(function(){
+    task = FlowGame.createTask({size: 1, progress: 1});
+    board = FlowGame.createBoard({
+      columns: [ FlowGame.createColumn({wip: 1}), FlowGame.createColumn({wip: 1}) ]
+    });
+  });
+
+  describe("first column contains one completed task, second column is empty", function() {
+
+    beforeEach(function() {
+      board = FlowGame.addTaskToBoard({ board: board, task: task, column: 0});
+    });
+
+    it("should move the task to the second column when we update the board", function() {
+      board = FlowGame.moveCompletedTasksOnBoard(board);
+      expect(board.columns[0].tasks).toBeEmpty();
+      expect(board.columns[1].tasks).toContain(task);
+    });
+  });
+});
+
